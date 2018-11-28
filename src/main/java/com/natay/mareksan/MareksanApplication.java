d02168e0fdfd5a2a16245f350bd1c00c723bfc03
Nurullah ATAY <natay78@gmail.com>
2018-11-28 19:41:22 +0300

    Initial commit

---
+++ src/main/java/com/natay/mareksan/MareksanApplication.java
@@ -0,0 +1,12 @@
+package com.natay.mareksan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MareksanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MareksanApplication.class, args);
+    }
+}
