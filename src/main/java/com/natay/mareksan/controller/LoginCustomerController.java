4244b5fb8ce4754440929d0d3a13da8b7c0d0fc7
ramazan.demir <ramazan.demir@trendyol.com>
2018-12-23 23:27:25 +0300

    refoctor duplicate code

--- src/main/java/com/natay/mareksan/controller/LoginCustomerController.java
+++ src/main/java/com/natay/mareksan/controller/LoginCustomerController.java
@@ -20,47 +20,47 @@ public class LoginCustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @RequestMapping(value={"/customerLogin","/"}, method = RequestMethod.GET)
-    public ModelAndView login(){
+    @RequestMapping(value = {"/customerLogin", "/"}, method = RequestMethod.GET)
+    public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ADMIN")) {
             modelAndView.setViewName("admin/userHome");
-        }else  if (roles.contains("CUSTOMER")){
+        } else if (roles.contains("CUSTOMER")) {
             modelAndView.setViewName("customer/customerHome");
-        }else {
+        } else {
             modelAndView.setViewName("customerLogin");
         }
         return modelAndView;
     }
 
 
+    @RequestMapping(value = "/customer/customerHome", method = RequestMethod.GET)
+    public ModelAndView homeCustomer() {
 
-    @RequestMapping(value="/customer/customerHome", method = RequestMethod.GET)
-    public ModelAndView homeCustomer(){
+        return getModelAndView(false);
+    }
+
+    @RequestMapping(value = "/admin/userHome", method = RequestMethod.GET)
+    public ModelAndView homeAdmin() {
+
+        return getModelAndView(true);
+
+    }
+
+    private ModelAndView getModelAndView(boolean isHomeAdmin) {
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Customer customer = customerService.findCustomerByAuthorizedEMail(auth.getName());
         modelAndView.addObject("userName", "Welcome " + customer.getAuthorizedName() + " " + customer.getAuthorizedPhone() + " (" + customer.getAuthorizedEMail() + ")");
-        modelAndView.addObject("adminMessage","Content Available Only for Users with Customer Role");
-        modelAndView.setViewName("customer/customerHome");
+        modelAndView.addObject("adminMessage", "Content Available Only for Users with Customer Role");
+        modelAndView.setViewName(isHomeAdmin ? "admin/userHome" : "customer/customerHome");
         return modelAndView;
     }
 
-    @RequestMapping(value="/admin/userHome", method = RequestMethod.GET)
-    public ModelAndView homeAdmin(){
-        ModelAndView modelAndView = new ModelAndView();
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Customer customer = customerService.findCustomerByAuthorizedEMail(auth.getName());
-        modelAndView.addObject("userName", "Welcome " + customer.getAuthorizedName() + " " + customer.getAuthorizedPhone() + " (" + customer.getAuthorizedEMail() + ")");
-        modelAndView.addObject("adminMessage","Content Available Only for Users with Customer Role");
-        modelAndView.setViewName("admin/userHome");
-        return modelAndView;
-    }
-
-    @RequestMapping(value={"/access_denied"}, method = RequestMethod.GET)
-    public ModelAndView access_denied(){
+    @RequestMapping(value = {"/access_denied"}, method = RequestMethod.GET)
+    public ModelAndView access_denied() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("access_denied");
         return modelAndView;
